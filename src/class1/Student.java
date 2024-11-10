ac72df3980403669b25f603c9ce39a8b7961d5da
HYEONGYUN\guddb <guddbs5545@naver.com>
2024-11-10 18:09:42 +0900

    ClassStart3 생성

--- src/class1/Student.java
+++ src/class1/Student.java
@@ -1,10 +1,9 @@
 package class1;
 
 public class Student {
+    String name;
+    int age;
+    int grade;
     public static void main(String[] args) {
-        String name;
-        int age;
-        int grade;
-
     }
 }
