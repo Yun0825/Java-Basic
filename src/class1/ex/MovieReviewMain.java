b4c641ba5aa0cee76cbe448f72802e2834fd3804
HYEONGYUN\guddb <guddbs5545@naver.com>
2024-11-24 14:26:18 +0900

    ex2.ProductOrder

--- src/class1/ex/MovieReviewMain.java
+++ src/class1/ex/MovieReviewMain.java
@@ -1,2 +1,21 @@
-package class1.ex;public class MovieReviewMain {
+package class1.ex;
+
+import class1.Student;
+
+public class MovieReviewMain {
+    public static void main(String[] args) {
+        MovieReview review1 = new MovieReview();
+        review1.title = "인셉션";
+        review1.review = "인생은 무한 루프";
+
+        MovieReview review2 = new MovieReview();
+        review2.title = "어바웃 타임";
+        review2.review = "인생 시간 영화!";
+
+        MovieReview[] reviews = {review1, review2};
+
+        for (MovieReview s : reviews) {
+            System.out.println("영화 제목: "+ s.title+", 리뷰: "+ s.review);
+        }
+    }
 }
